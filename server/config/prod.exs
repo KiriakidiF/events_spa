9381c0d6c1781e15beda4d136ea11be06409f58c
Ted Kiriakidi <fedorkiriakidi@gmail.com>
2021-03-26 14:23:51 -0400

    Updated prod configs

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -1,5 +1,11 @@
 use Mix.Config
 
+config :cors_plug,
+  origin: ["http://events-spa.tkwaffle.site"],
+  max_age: 86400,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  headers: ["x-auth", "content-type"]
+
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
@@ -10,7 +16,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :events_server, EventsServerWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- server/config/prod.secret.exs
+++ server/config/prod.secret.exs
@@ -4,6 +4,7 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
+
 database_url =
   System.get_env("DATABASE_URL") ||
     raise """
@@ -35,7 +36,7 @@ config :events_server, EventsServerWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :events_server, EventsServerWeb.Endpoint, server: true
+config :events_server, EventsServerWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
